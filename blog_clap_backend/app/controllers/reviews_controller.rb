55e799090187a849a678c666445852275a8e66c1
estherkimyunjung <estherkimyunjung@gmail.com>
2020-06-15 18:43:49 -0500

    create js files

--- blog_clap_backend/app/controllers/reviews_controller.rb
+++ blog_clap_backend/app/controllers/reviews_controller.rb
@@ -13,5 +13,13 @@ class ReviewsController < ApplicationController
     render json: ReviewSerializer.new(reviews)
   end
   
-  
+  def create 
+    new_review = Review.create(new_review_params)
+    render json:ReviewSerializer.new(review)
+  end
+
+  private 
+  def new_review_params
+    params.require(:review).permit(:blog_id, :blogger_id, :clap, :comment, :created_at)
+  end
 end
